b2ebe781934a07a85e061d153b3552750955e9bb
Jaesonb <jaesonbernardsha@gmail.com>
2024-07-09 11:51:41 +0530

    Revert "v1"
    This reverts commit 312dddb0de5ca0bc328067bf5502c7d4c42d41fc.

--- js/questions.js
+++ js/questions.js
@@ -1,36 +1,36 @@
 // creating an array and passing the number, questions, options, and answers
 let questions = [
-    {
-    numb: 1,
-    question: "Friday variya?",
-    answer: "Yes",
-    options: [
-      "Yes",
-      "Maybe Yes",
-      "No, but pakla",
-      "pakla"
-    ]
-  },
-    {
-    numb: 2,
-    question: "Nee eppa taa free?",
-    answer: "This Friday",
-    options: [
-      "This Friday",
-      "Maybe this Friday",
-      "Amma kitta keknu",
-      "Anytime 😏"
-    ]
-  },
-    {
-    numb: 3,
-    question: "Saptiya?",
-    answer: "Friday velila kondu po",
-    options: [
-      "Sapte",
-      "Inu illa",
-      "Friday velila kondu po",
-      "Amma seiyonum"
-    ]
-  },
+  {
+  numb: 1,
+  question: "What does social engineering mean?",
+  answer: "Using socially available mediums to reveal or identify specific information",
+  options: [
+    "Engineering something for social benefit",
+    "Engineering with the community",
+    "Using socially available mediums to reveal or identify specific information",
+    "I don't know"
+  ]
+},
+  {
+  numb: 2,
+  question: "Best practice for password?",
+  answer: "Store it in the password manager only",
+  options: [
+    "Write it in a paper",
+    "Store it in the password manager only",
+    "Say it to friends",
+    "I don't know"
+  ]
+},
+  {
+  numb: 3,
+  question: "What do you do when you get a suspicious email?",
+  answer: "Report it to the security team",
+  options: [
+    "Report it to the security team",
+    "Don't inform anyone",
+    "Click on any urls inside the email",
+    "I don't know"
+  ]
+},
 ];
